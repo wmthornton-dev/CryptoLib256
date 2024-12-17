377695c0123e71b4dda6d6acd4ccbf28e89243cf
Wayne Thornton <wmthornton-dev@outlook.com>
2024-12-16 17:31:56 -0700

    Updated file header in sha256.cpp

--- sha256.cpp
+++ sha256.cpp
@@ -1,12 +1,12 @@
 /*
- * This file is part of the CryptoLib256 library.
- * Developed (D) 2021/2022 by Wayne Michael Thornton aka WMT.
- * 
+ * This file is part of software which has been
+ * Developed (D) 2024 by Wayne Michael Thornton aka WMT.
+ *
  * Distributed under the Public Benefit Zero Copyright License (v.1.0)
- * 
+ *
  * You should have received a copy of the Public Benefit Zero Copyright License
  * along with this program. If not, see <https://github.com/wmthornton/PBZC.git>.
-*/
+ */
 
 #include "CryptoLib256/CryptoLib256.h"
 
